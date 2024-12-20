e1480d9bc7ee3f3555465721d46534b27e42ff9a
Pranav More <pranavmore96k@gmail.com>
2024-12-20 23:31:47 +0530

    Bolierplate Setup

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import styles from './Footer.module.css'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+        <div>@2024 Lamania.All rights reserved</div>
+        <div>
+            <div className={styles.images}>
+              <Image src="/1.png"  height={15} width={15} className={styles.icon} alt='Lama Dev' ></Image>
+              <Image src="/2.png"  height={15} width={15} className={styles.icon} alt='Lama Dev' ></Image>
+              <Image src="/3.png"  height={15} width={15} className={styles.icon} alt='Lama Dev' ></Image>
+              <Image src="/4.png"  height={15} width={15} className={styles.icon} alt='Lama Dev' ></Image>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
